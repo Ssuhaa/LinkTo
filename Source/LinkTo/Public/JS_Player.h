0156b9259b99ed712ef4f5a18189a5ec919250d1
choi <jongseobchoi@gmail.com>
2023-02-01 17:53:06 +0900

    1830

---
+++ Source/LinkTo/Public/JS_Player.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+#include "JS_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINKTO_API AJS_Player : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LinkTo/Public/LinkToGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LinkToGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINKTO_API ALinkToGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
